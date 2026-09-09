dda05016c7b5b26ad4cb56ba520507e1417f4d28
Harsh <jangraharsh213@gmail.com>
2026-09-09 18:12:50 +0530

    fixed minor bugs

--- Library_Management/Library_Management/settings.py
+++ Library_Management/Library_Management/settings.py
@@ -125,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'library/static/library/css')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'library/static/')]
--- Library_Management/library/views.py
+++ Library_Management/library/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse,JsonResponse
-from library.models import *
+from library.models import Book, Borrow_record
 from django.contrib.auth import login,authenticate,logout,get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -42,28 +42,6 @@ def logout_view(request):
     logout(request)
     return redirect("login")
 
-@login_required(login_url="/login/")
-def record_view(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    
-    if not book.available:
-        return HttpResponse("The book is not available")
-    
-    date = request.POST["date"]
-
-    record = Borrow_record()
-
-    record.expected_return_date = date
-    record.book = book
-    record.user = request.user
-
-    book.available = False
-
-    book.save()
-    record.save()
-
-    return redirect("home")
-
 def register(request):
     if request.method == "POST":
         fname= request.POST["fname"]
@@ -91,10 +69,34 @@ def register(request):
     return render(request, 'register.html')
 
 @login_required(login_url="/login/")
+def record_view(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    
+    if not book.available:
+        return HttpResponse("The book is not available")
+    
+    if request.method == "POST":
+        date = request.POST["date"]
+
+        record = Borrow_record()
+
+        record.expected_return_date = date
+        record.book = book
+        record.user = request.user
+
+        book.available = False
+
+        book.save()
+        record.save()
+
+        return redirect("home")
+    return HttpResponse("invaild request method")
+
+@login_required(login_url="/login/")
 def return_book(request, book_id):
     if request.method == "POST":
         book = get_object_or_404(Book, id=book_id)
-        # Find the active borrow record for this user and book
+        # Find the exact borrow record for this user and book
         record = Borrow_record.objects.filter(user=request.user, book=book, return_date__isnull=True).first()
         
         if record:
